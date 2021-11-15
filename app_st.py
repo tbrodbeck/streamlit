49b8be5e62edb7f732452a677fb39cd5733d9151
Anna Istomina <anna.istomina@ibm.com>
2021-11-15 20:52:43 +0100

    initial version with video

--- app_st.py
+++ app_st.py
@@ -1,5 +1,6 @@
 import streamlit as st
 import pandas as pd
+from streamlit_player import st_player
 
 # ----------------------------------------Basic configs------------------------------------------------------------------
 st.set_page_config(layout="wide")
@@ -25,29 +26,48 @@ reg_user = sd_bar.radio("Are you registered user?", ["Yes", "No"])
 
 # to check if the user is registered, then to ask for id
 if reg_user == "Yes":
-    param_0 = sd_bar.text_input("Enter you id here")
+    with sd_bar.form(key="user_form"):
+        user_id = st.text_input("Enter you id here")
+        submit_button = st.form_submit_button(label="Submit")
 else:
     param_0 = sd_bar.checkbox("I do not want to fill your inputs", False)
+    # no need to submit the form then
+    submit_button = True
+    if not param_0:
+        with sd_bar.form(key="user_form"):
+            # to add from data
+            age_list = dict(
+                {0: "<18",
+                 1: "18-40",
+                 2: "40-65",
+                 3: ">65"}
+            )
+            age_input = st.radio("Age group", age_list.values())
 
-    if param_0:
-        # to add from data
-        param_1_list = dict(
-            {0: "<18",
-             1: "18-40",
-             2: "40-65",
-             3: ">65"}
-        )
-        param_1 = sd_bar.radio("Age group", param_1_list.values())
-
-        param_2_list = ['Drama', 'Horror', 'Romantic']
-        param_2 = sd_bar.multiselect("Select your favourite genres",
-                                   param_2_list,
-                                   )
-        param_3 = sd_bar.slider("Maximum length in mins", 0, 300)
+            genres_list = ['Drama', 'Horror', 'Romantic']
+            genres_input = st.multiselect("Select your favourite genres",
+                                       genres_list,
+                                       )
+            param_3 = st.slider("Maximum length in mins", 0, 300)
+            submit_button = st.form_submit_button(label="Submit")
 
 
 #------------------------ OUTPUTS------------------------------------
-st.markdown("### Initial prediction")
 
-st.markdown("Harry Potter")
-st.markdown("[Link to imdb] (https://www.imdb.com/title/tt0241527/?ref_=tt_mv_close)")
+if not submit_button:
+    st.markdown('<h3 style="color:red">Please enter the data on the left panel and submit</h3>',
+                unsafe_allow_html=True)
+else:
+    st.markdown("### Initial prediction is")
+    st.markdown('<h3 style="color:violet">Harry Potter</h3>', unsafe_allow_html=True)
+    st.markdown("### *Youtube search results*")
+    # to get embedded youtube video
+    # enter https://codefather.tech/blog/youtube-search-python/
+    """ query is like this
+    https: // www.youtube.com / results?search_query = harry + potter + trailer"""
+    st_player('https://www.youtube.com/watch?v=VyHV0BRtdxo')
+    submit_button = False
+
+    # to get embedded youtube video
+
+
